90d5ce0f638b8c6b3c6d5b333e36fd6bb2c4ed82
zingz0r <zingz0r@tothnet.hu>
2019-10-27 01:33:12 +0200

    idk forgotto commit

--- StatusPage.Api/HostedServices/CakeUpdaterHostedService.cs
+++ StatusPage.Api/HostedServices/CakeUpdaterHostedService.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using StatusCake.Client;
 using StatusCake.Client.Interfaces;
+using StatusPage.Api.Config;
 using StatusPage.Api.Interfaces;
 
 namespace StatusPage.Api.HostedServices
@@ -12,16 +17,18 @@ namespace StatusPage.Api.HostedServices
 
     internal class CakeUpdaterHostedService : IHostedService, IDisposable
     {
-        private readonly IStatusCakeClient _statusCakeClient;
+        private readonly ILogger<CakeUpdaterHostedService> _logger;
         private readonly ITestsModel _testsModel;
         private readonly IAvailabilityModel _availabilityModel;
+        private readonly StatusCakeConfig _config;
         private Timer _timer;
 
-        public CakeUpdaterHostedService(IStatusCakeClient statusCakeClient, ITestsModel testsModel, IAvailabilityModel availabilityModel)
+        public CakeUpdaterHostedService(ILogger<CakeUpdaterHostedService> logger, ITestsModel testsModel, IAvailabilityModel availabilityModel, StatusCakeConfig config)
         {
-            _statusCakeClient = statusCakeClient;
+            _logger = logger;
             _testsModel = testsModel;
             _availabilityModel = availabilityModel;
+            _config = config;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
@@ -34,19 +41,35 @@ namespace StatusPage.Api.HostedServices
 
         private void UpdateTestsModel(object state)
         {
-            var tests = _statusCakeClient.GetTestsAsync().Result;
-
-            _availabilityModel.Lock();
-
-            foreach (var test in tests)
+            try
             {
-                _testsModel.UpdateTest(test);
-                var availabilities = _statusCakeClient.GetUptimesAsync(test.TestID).Result;
+                using (var statusCakeClient = new StatusCakeClient(_config.UserName, _config.ApiKey))
+                {
+                    _logger.LogInformation("Started to update availabilities");
+                    var watch = new Stopwatch();
+                    watch.Start();
+                    var tests = statusCakeClient.GetTestsAsync().Result;
 
-                _availabilityModel.UpdateAvailability(test.TestID, availabilities);
+                    _availabilityModel.Lock();
+
+                    foreach (var test in tests)
+                    {
+                        _testsModel.UpdateTest(test);
+                        var availabilities = statusCakeClient.GetUptimesAsync(test.TestID).Result;
+
+                        _availabilityModel.UpdateAvailability(test.TestID, availabilities);
+                    }
+
+                    _availabilityModel.Unlock();
+                    watch.Stop();
+                    _logger.LogInformation($"Finished to update availabilities. It took {watch.Elapsed.TotalMilliseconds} ms.");
+
+                }
             }
-
-            _availabilityModel.Unlock();
+            catch
+            {
+                _logger.LogError("No internet connection?");
+            }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
